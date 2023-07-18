8947a8a1863f1bf471c56e9458ca82e39bba4f96
TheGiant <thegiantgamesindia@gmail.com>
2023-07-18 22:18:49 +0530

    updated

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def print():
+    return "in print"
+
+if __name__ == '__main__':
+    app.run(debug=True)
