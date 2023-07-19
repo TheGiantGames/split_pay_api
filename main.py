2d116d71c4e5c646c1a08a6e66d249b89466539c
TheGiant <thegiantgamesindia@gmail.com>
2023-07-19 11:44:54 +0530

    updated

--- main.py
+++ main.py
@@ -1,13 +1,64 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
-
+# Init App
 app = Flask(__name__)
+app.app_context().push()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/')
-def print():
-    return "in print"
+# Database Config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'user.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init marshmallow
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __int__(self, username = username, password= password, email=  email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'username', 'password', 'email')
+
+
+# init Schema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+@app.route('/', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.json['username']
+        password = request.json['password']
+        email = request.json['email']
+
+        new_user = User(username=username,password= password,email= email)
+        db.session.add(new_user)
+        db.session.commit()
+        return user_schema.jsonify(new_user)
+
+
+
+@app.route('/user')
+def u():
+    return "get method"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
